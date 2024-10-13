4069aa10395b576be62c9d37d9f07af84827d52c
Eduard <kozeychukk1997@gmail.com>
2024-10-13 19:03:45 +0300

    fixe Certificate page heightt for mobile

--- src/components/Certificates/Certificates.jsx
+++ src/components/Certificates/Certificates.jsx
@@ -31,21 +31,21 @@ const certificates = [
         title: "Hillel IT School",
         description:
             "Сертифікат про проходження курсу 'Вступ до Python' у Hillel IT School.",
-        file: HillelPDF, // Путь к PDF-файлу
+        file: HillelPDF,
     },
     {
         image: EnITStepPageOne,
         title: "Step IT Academy (English)",
         description:
             "Сертифікат про завершення курсу Frontend Developer на англійській мові.",
-        file: EnITStepPDF, // Путь к PDF-файлу
+        file: EnITStepPDF,
     },
     {
         image: UkITStepPageOne,
         title: "Step IT Academy (Ukrainian)",
         description:
             "Сертифікат про завершення курсу Frontend Developer українською мовою.",
-        file: UkITStepPDF, // Путь к PDF-файлу
+        file: UkITStepPDF,
     },
 ];
 
@@ -53,16 +53,20 @@ export const Certificates = () => {
     const [open, setOpen] = useState(false);
     const [selectedCertificate, setSelectedCertificate] = useState(null);
     const [divHeight, setDivHeight] = useState(window.innerHeight);
+    const [contentLoaded, setContentLoaded] = useState(false); // Состояние для контроля отображения контента
 
     useEffect(() => {
         const handleResize = () => {
-            const newHeight =
-                window.innerHeight < 900 ? 950 : window.innerHeight;
+            const newHeight = window.innerHeight < 900 ? 950 : window.innerHeight;
             setDivHeight(newHeight);
+            setContentLoaded(true); // Устанавливаем состояние в true после вычисления высоты
         };
 
         // Добавляем слушателя события resize
         window.addEventListener("resize", handleResize);
+
+        // Устанавливаем начальную высоту и показываем контент
+        handleResize();
 
         // Убираем слушателя события при размонтировании
         return () => {
@@ -81,60 +85,62 @@ export const Certificates = () => {
     };
 
     return (
-        <div className='certificate-conteyner'>
+        <div className='certificate-container'>
             <Header />
 
-            <div className='certificate-box' style={{ height: divHeight }}>
-                {certificates.map((certificate, i) => (
-                    <Card
-                        sx={{
-                            maxWidth: 745,
-                            height: "max-content",
-                            position: "relative",
-                        }}
-                        key={i}
-                    >
-                        <CardActionArea>
-                            <CardMedia
-                                component='img'
-                                height='140'
-                                image={certificate.image}
-                                alt={certificate.title}
-                            />
+            {contentLoaded && ( // Проверяем состояние загрузки контента
+                <div className='certificate-box' style={{ height: divHeight }}>
+                    {certificates.map((certificate, i) => (
+                        <Card
+                            sx={{
+                                maxWidth: 745,
+                                height: "max-content",
+                                position: "relative",
+                            }}
+                            key={i}
+                        >
+                            <CardActionArea>
+                                <CardMedia
+                                    component='img'
+                                    height='140'
+                                    image={certificate.image}
+                                    alt={certificate.title}
+                                />
 
-                            <IconButton
-                                aria-label='settings'
-                                onClick={() => handleClickOpen(certificate)}
-                                sx={{
-                                    position: "absolute",
-                                    top: 15,
-                                    right: 0,
-                                    zIndex: 1, // Чтобы кнопка была выше остальных элементов4
-                                    color: "black",
-                                }}
-                            >
-                                <MoreVertIcon />
-                            </IconButton>
+                                <IconButton
+                                    aria-label='settings'
+                                    onClick={() => handleClickOpen(certificate)}
+                                    sx={{
+                                        position: "absolute",
+                                        top: 15,
+                                        right: 0,
+                                        zIndex: 1,
+                                        color: "black",
+                                    }}
+                                >
+                                    <MoreVertIcon />
+                                </IconButton>
 
-                            <CardContent>
-                                <Typography
-                                    gutterBottom
-                                    variant='h5'
-                                    component='div'
-                                >
-                                    {certificate.title}
-                                </Typography>
-                                <Typography
-                                    variant='body2'
-                                    sx={{ color: "text.secondary" }}
-                                >
-                                    {certificate.description}
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
-                ))}
-            </div>
+                                <CardContent>
+                                    <Typography
+                                        gutterBottom
+                                        variant='h5'
+                                        component='div'
+                                    >
+                                        {certificate.title}
+                                    </Typography>
+                                    <Typography
+                                        variant='body2'
+                                        sx={{ color: "text.secondary" }}
+                                    >
+                                        {certificate.description}
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    ))}
+                </div>
+            )}
 
             {/* Модальное окно */}
             {selectedCertificate && (
@@ -144,8 +150,7 @@ export const Certificates = () => {
                             {selectedCertificate.title}
                         </DialogContentText>
                         <DialogContentText>
-                            Нажмите на одну из кнопок ниже, чтобы открыть или
-                            скачать сертификат.
+                            Нажмите на одну из кнопок ниже, чтобы открыть или скачать сертификат.
                         </DialogContentText>
                     </DialogContent>
                     <DialogActions>
@@ -159,11 +164,11 @@ export const Certificates = () => {
                         <Button
                             onClick={() => {
                                 const link = document.createElement("a");
-                                link.href = selectedCertificate.file; // Путь к файлу
-                                link.setAttribute("download", ""); // Атрибут для скачивания
+                                link.href = selectedCertificate.file; 
+                                link.setAttribute("download", "");
                                 document.body.appendChild(link);
-                                link.click(); // Симуляция клика по ссылке
-                                document.body.removeChild(link); // Удаление ссылки после скачивания
+                                link.click();
+                                document.body.removeChild(link);
                             }}
                         >
                             Скачать PDF
